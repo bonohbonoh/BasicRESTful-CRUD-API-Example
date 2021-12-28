0c4c6540fc98af5b2e70a9d413596e6ca9c8d005
bonohbonoh <psmb2006@naver.com>
2021-12-28 16:20:49 +0900

    Initial commit

---
+++ src/main/java/com/example/BasicRESTfulCRUDAPIExample/BasicResTfulCrudApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.BasicRESTfulCRUDAPIExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicResTfulCrudApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicResTfulCrudApiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BasicRESTfulCRUDAPIExample/BasicResTfulCrudApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicRESTfulCRUDAPIExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicResTfulCrudApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
